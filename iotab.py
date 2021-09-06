1f391880160f0b7a213a2b5119b895116ca66ea2
Anita Orhand <anita.orhand@gmail.com>
2021-09-06 22:54:39 +0200

    Generate A0 and A4

--- iotab.py
+++ iotab.py
@@ -32,6 +32,25 @@ class IOTab(scrolled.ScrolledPanel):
         newline.Add(self.output_fname_display, flag=wx.ALIGN_CENTRE_VERTICAL|wx.LEFT, border=5)
         vert_sizer.Add(newline,flag=wx.TOP|wx.LEFT|wx.RIGHT,border=10)
 
+        self.generate_a0 = wx.CheckBox(self,label=_('Generate A0'))
+        self.generate_a0.SetValue(1)
+        self.generate_a0.Bind(wx.EVT_CHECKBOX,self.on_generate_a0a4_checked)
+        vert_sizer.Add(self.generate_a0,flag=wx.TOP|wx.LEFT|wx.RIGHT,border=10)
+        
+        newline = wx.BoxSizer(wx.HORIZONTAL)
+        self.generate_a4 = wx.CheckBox(self,label=_('Generate A4'))
+        self.generate_a4.SetValue(1)
+        self.generate_a4.Bind(wx.EVT_CHECKBOX,self.on_generate_a0a4_checked)
+        newline.Add(self.generate_a4, flag=wx.ALIGN_CENTRE_VERTICAL)
+        self.in_canvas_doc_btn = wx.Button(self, label=_('Select input A4 SVG canvas'))
+        self.in_canvas_doc_btn.Bind(wx.EVT_BUTTON,main_gui.on_open_canvas)
+        newline.Add(self.in_canvas_doc_btn, flag=wx.ALIGN_CENTRE_VERTICAL)
+        self.input_canevas_fname_display = wx.StaticText(self, label=_('None'))
+        newline.Add(self.input_canevas_fname_display, flag=wx.ALIGN_CENTRE_VERTICAL|wx.LEFT, border=5)
+        vert_sizer.Add(newline,flag=wx.TOP|wx.LEFT|wx.RIGHT,border=10)      
+        
+        self.on_generate_a0a4_checked(event=None)
+
         self.SetSizer(vert_sizer)
         self.SetupScrolling()
         self.SetBackgroundColour(parent.GetBackgroundColour())
@@ -39,4 +58,13 @@ class IOTab(scrolled.ScrolledPanel):
     def load_new(self,filename):
         self.input_fname_display.SetLabel(filename)
         self.output_fname_display.SetLabel(label=_('None'))
+        
+    def load_new_canvas(self,filename):
+        self.input_canevas_fname_display.SetLabel(filename)
          
+    def on_generate_a0a4_checked(self,event):
+        self.generate_a0_checked = bool(self.generate_a0.GetValue())
+        self.generate_a4_checked = bool(self.generate_a4.GetValue())
+        self.in_canvas_doc_btn.Enable(self.generate_a4_checked)
+        
+        
