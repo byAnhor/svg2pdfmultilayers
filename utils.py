80417f8278b52e9ee919ccb240dbcee92ec557ce
Anita Orhand <anita.orhand@gmail.com>
2021-11-10 10:10:00 +0100

    Revert "essai"
    This reverts commit f0deb7f2c1ecd024892dbc7dc4343ccde6a59dee.

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def resource_path(relative_path):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     else:
-        base_path = os.path.abspath('.//ressources')
+        base_path = os.path.abspath('.')
 
     return os.path.join(base_path, relative_path)
 
